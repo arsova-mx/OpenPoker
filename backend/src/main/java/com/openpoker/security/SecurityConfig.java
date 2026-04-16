0a69985ba8f63f08ce508ba113797c2ce5e91ac6
Wilfredo Pardo <wilfredo.pardo@arsova.mx>
2026-04-16 13:13:38 -0600

    Commit: Primer issue modificado: cambio en la filterchain almomento de habilitar formLogin/httpBasic

--- backend/src/main/java/com/openpoker/security/SecurityConfig.java
+++ backend/src/main/java/com/openpoker/security/SecurityConfig.java
@@ -20,8 +20,9 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable()).sessionManagement(session -> session.sessionCreationPolicy(
-                SessionCreationPolicy.STATELESS)).authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**",
-                "/api/hello").permitAll().requestMatchers("/api/**").authenticated()).addFilterBefore(jwtFilter,
+                SessionCreationPolicy.STATELESS)).formLogin(form -> form.disable()).httpBasic(basic ->
+                basic.disable()).authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**", "/api/hello")
+                .permitAll().requestMatchers("/api/**").authenticated()).addFilterBefore(jwtFilter,
                 UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
