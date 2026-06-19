fd760125abe203654fc5a5b4746a3bac61b35e8e
MarcoARSA <marco.elenes@arsova.mx>
2026-06-19 14:37:00 -0600

    cree las tablas tickets y comments

--- backend/src/main/java/com/openpoker/entity/Comments.java
+++ backend/src/main/java/com/openpoker/entity/Comments.java
@@ -1,5 +1,30 @@
 package com.openpoker.entity;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ticket")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Comments {
+
+    @Id
+    private UUID id;
+
+    private UUID ticket_id;
+
+    
 
 }
--- backend/src/main/java/com/openpoker/entity/Ticket.java
+++ backend/src/main/java/com/openpoker/entity/Ticket.java
@@ -1,5 +1,47 @@
 package com.openpoker.entity;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ticket")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Ticket {
+
+    @Id
+    private UUID id;
+
+    @ManyToOne(fetch =FetchType.LAZY)
+    @JoinColumn(name="id_session", nullable = false)
+    private GameSession gameSession;
+
+    @Column(name="title", nullable = false,length = 255)
+    private String tittle;
+
+    @Column(name="description",length = 500)
+    private String description;
+
+
+
+
+
+
+    
 
 }
